b5f9443a089a69e8cf5c5f3c29f4509d81f02d75
JerryWen <wen18840839539@gmail.com>
2021-01-07 11:52:15 +0800

    Init SRP

---
+++ Assets/Custom RP/CameraRenderer.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+/// <summary>
+/// 每个相机单独渲染，为了方便扩展使用，比如人物相机，地图相机，forward rendering, deferred rendering;
+/// </summary>
+public class CameraRenderer
+{
+
+    ScriptableRenderContext context;
+
+    Camera camera;
+
+    /// <summary>
+    /// 渲染所有相机可见部分
+    /// </summary>
+    /// <param name="context"></param>
+    /// <param name="camera"></param>
+    public void Render(ScriptableRenderContext context, Camera camera)
+    {
+        this.context = context;
+        this.camera = camera;
+
+        DrawVisibleGeometry();
+        Submit();
+    }
+    void DrawVisibleGeometry()
+    {
+        context.DrawSkybox(camera);
+    }
+    /// <summary>
+    /// 将 buffere 提交给 context
+    /// </summary>
+    void Submit()
+    {
+        context.Submit();
+    }
+}
---
+++ Assets/Custom RP/CustomRenderPipeline.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CustomRenderPipeline : RenderPipeline
+{
+
+    CameraRenderer renderer = new CameraRenderer();
+
+    protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+    {
+        foreach (Camera camera in cameras) // 渲染所有Camera
+        {
+            renderer.Render(context, camera);
+        }
+    }
+}
