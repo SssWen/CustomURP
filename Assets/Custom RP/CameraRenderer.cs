fee2db22290e2d7e67a73c97e0afa60c17b3aceb
JerryWen <wen18840839539@gmail.com>
2021-01-08 09:10:15 +0800

    Unlit renderer pipeline.

--- Assets/Custom RP/CameraRenderer.cs
+++ Assets/Custom RP/CameraRenderer.cs
@@ -6,11 +6,14 @@ using UnityEngine.Rendering;
 /// </summary>
 public class CameraRenderer
 {
-
+    const string bufferName = "Render Camera";
+    static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
+    // 天空盒可独立的buffer，其他的Geometry需要使用commandbuffer定义
+    CommandBuffer buffer = new CommandBuffer{ name = bufferName };
     ScriptableRenderContext context;
 
     Camera camera;
-
+    CullingResults cullingResults;
     /// <summary>
     /// 渲染所有相机可见部分
     /// </summary>
@@ -20,19 +23,82 @@ public class CameraRenderer
     {
         this.context = context;
         this.camera = camera;
+        // 剔除相机外的物体
+        if (!Cull())
+        {
+            return;
+        }
 
+        Setup();
         DrawVisibleGeometry();
         Submit();
     }
+    /// <summary>
+    /// 渲染可被相机看见的物体,顺序 opaque->skybox->transparent
+    /// </summary>
     void DrawVisibleGeometry()
     {
-        context.DrawSkybox(camera);
+        // 不透明物体
+        var sortingSettings = new SortingSettings(camera) // 渲染顺序 front to back
+        {
+            criteria = SortingCriteria.CommonOpaque 
+        };        
+        var drawingSettings = new DrawingSettings(
+            unlitShaderTagId, sortingSettings
+        );
+        var filteringSettings = new FilteringSettings(RenderQueueRange.all);
+        context.DrawRenderers(
+            cullingResults, ref drawingSettings, ref filteringSettings
+        );
+        // skybox
+        context.DrawSkybox(camera);//天空盒比较特殊，可以直接使用，渲染其他几何物体要用CommandBuffer
+
+        // 透明物体
+        sortingSettings.criteria = SortingCriteria.CommonTransparent; //渲染顺序 back to front
+        drawingSettings.sortingSettings = sortingSettings;
+        filteringSettings.renderQueueRange = RenderQueueRange.transparent;
+        context.DrawRenderers(
+            cullingResults, ref drawingSettings, ref filteringSettings
+        );
     }
     /// <summary>
-    /// 将 buffere 提交给 context
+    /// 设置好Camera相关矩阵,unity_MatrixVP 等
+    /// </summary>
+    void Setup()
+    {
+        // inject profiler samples
+        buffer.ClearRenderTarget(true, true, Color.clear);//每次进行渲染都需要清除一次，增加一次 DrawGL
+        buffer.BeginSample(bufferName);
+        ExecuteBuffer();
+        context.SetupCameraProperties(camera);
+    }
+    /// <summary>
+    /// 最后一步，将 buffer 提交给 context
     /// </summary>
     void Submit()
     {
+        buffer.EndSample(bufferName);
+        ExecuteBuffer();
         context.Submit();
     }
+
+    /// <summary>
+    /// 执行buffer
+    /// </summary>
+    void ExecuteBuffer()
+    {
+        context.ExecuteCommandBuffer(buffer);
+        buffer.Clear();
+    }
+
+    bool Cull()
+    {
+        if (camera.TryGetCullingParameters(out ScriptableCullingParameters p))
+        {
+            cullingResults = context.Cull(ref p);
+            return true;
+        }
+        return false;
+    }
+
 }
